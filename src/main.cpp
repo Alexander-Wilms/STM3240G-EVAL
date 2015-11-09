4c30309abc4d3f01092f2a2d27c010474e912a31
stfawilm <stfawilm@f15fa749-dd57-4e2c-bc1e-1bdf2953ec43>
2015-11-09 11:47:05 +0000

    Kooperatives Multitaskting
    git-svn-id: https://tools.eit.h-da.de/svn/stfawilm/rzs2_stm32f4/trunk@18 f15fa749-dd57-4e2c-bc1e-1bdf2953ec43

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,13 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void init_system (void);
+#include <string>
+#include <sstream>
 
+volatile int stuff;
+
+
+void init_system (void);
 
 void test_task (void *)
 {
@@ -39,7 +44,73 @@ void test_task (void *)
     }
 }
 
-#define TEST_TASK_PRIORITY ((1 + tskIDLE_PRIORITY) | portPRIVILEGE_BIT)
+void led1_task (void *)
+{
+  BSP_LED_Init (LED4);
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+  while (true)
+    {
+	  stuff = 0;
+      BSP_LED_Toggle(LED4);
+      // Task wird erst in 100 Ticks weiter ausgeführt
+      //vTaskDelay ((int)1000/3);
+      for( volatile long int counter = 0;counter<1000000;counter++)
+  		;
+      BSP_LED_Toggle(LED4);
+      //vTaskDelay ((int)2000/3);
+      for( volatile long int counter = 0;counter<2000000;counter++)
+        ;
+      taskYIELD();
+    }
+}
+
+void led2_task (void *)
+{
+  BSP_LED_Init (LED3);
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+  while (true)
+    {
+	  stuff = 0;
+      BSP_LED_Toggle(LED3);
+      // Task wird erst in 100 Ticks weiter ausgeführt
+      //vTaskDelay ((int)1000/3);
+      for( volatile long int counter = 0;counter<1000000;counter++)
+  		;
+      BSP_LED_Toggle(LED3);
+      //vTaskDelay ((int)2000/3);
+      for( volatile long int counter = 0;counter<2000000;counter++)
+        ;
+      taskYIELD();
+    }
+}
+
+void lcd_task (void *)
+{
+	std::stringstream output;
+	std::string outputstring;
+	const char * chararray;
+	int i = 0;
+	for(;;)
+	{
+		stuff = 0;
+		output.str(std::string());
+		output << "Counter: " << i%1000;
+		outputstring = "";
+		outputstring = output.str();
+		chararray = "";
+		chararray = outputstring.c_str();
+		lcd_write_line (1, (char *) chararray);
+		for( volatile long int counter = 0;counter<1000000;counter++)
+		  		;
+		i++;
+		taskYIELD();
+	}
+}
+
+#define LED_TASK_PRIORITY ((1 + tskIDLE_PRIORITY) | portPRIVILEGE_BIT)
+#define LCD_TASK_PRIORITY ((1 + tskIDLE_PRIORITY) | portPRIVILEGE_BIT)
 
 /** @brief main function, entry point of the application */
 int
@@ -51,7 +122,10 @@ main (void)
   lcd_init ();
   lcd_write_line (0, "Hello FreeRTOS");
 
-  xTaskCreate( (pdTASK_CODE)test_task, 	"test", configMINIMAL_STACK_SIZE, 0, TEST_TASK_PRIORITY, NULL);
+  //xTaskCreate( (pdTASK_CODE)test_task, 	"test", configMINIMAL_STACK_SIZE, 0, TEST_TASK_PRIORITY, NULL);
+  xTaskCreate( (pdTASK_CODE)led1_task, 	"led1", 256, 0, LED_TASK_PRIORITY, NULL);
+  xTaskCreate( (pdTASK_CODE)led2_task, 	"led2", 256, 0, LED_TASK_PRIORITY, NULL);
+  xTaskCreate( (pdTASK_CODE)lcd_task, 	"lcd", 256, 0, LCD_TASK_PRIORITY, NULL);
 
   vTaskStartScheduler ();
 }
