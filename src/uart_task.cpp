c1baa24ae40b992bd4f1d38baae100c1741fc428
stfawilm <stfawilm@f15fa749-dd57-4e2c-bc1e-1bdf2953ec43>
2016-01-11 11:22:19 +0000

    CAN transmit funktioniert
    git-svn-id: https://tools.eit.h-da.de/svn/stfawilm/test_stm32f4_5/trunk@33 f15fa749-dd57-4e2c-bc1e-1bdf2953ec43

--- src/uart_task.cpp
+++ src/uart_task.cpp
@@ -15,23 +15,33 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "candriver.h"
+
 ROM char message[]="hello via UART\r\n";
 
 uart uart3;
+
+extern COMMON can_driver_t *can;
 
 /** @brief  demo task demonstrating USART usage */
 void uart_task( void *)
 {
 	char output[2] = {0, 0};
-  char buffer[30]={0};
+	char buffer[30]={0};
 
+	can_driver_t::CANpacket txcanpk;
+	uint8_t tx[8] = "ABCDEFG";
+	for(int i = 0; i <=8;i++)
+	{
+		txcanpk.data_b[i] =  tx[i];
+	}
 
-  uart3.puts(message);
+	uart3.puts(message);
 
-  int i = 0;
+	int i = 0;
 
-  while( true)
-    {
+	while( true)
+	{
 		while(i < 30)
 		{
 			uart3.wait_4_character();
@@ -45,6 +55,7 @@ void uart_task( void *)
 
 		BSP_LCD_ClearStringLine(1);
 		lcd_write_line(1, buffer);
+		can->send(txcanpk);
 		i = 0;
-    }
+	}
 }
